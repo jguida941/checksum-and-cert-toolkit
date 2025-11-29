a53c6557c2d9abc6f0644a6d107b28460b55491b
Justin <justin.guida@snhu.edu>
2025-11-29 05:43:11 -0500

    Tidy test import spacing

--- src/test/java/com/snhu/sslserver/ServerApplicationTests.java
+++ src/test/java/com/snhu/sslserver/ServerApplicationTests.java
@@ -2,7 +2,6 @@ package com.snhu.sslserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.snhu.sslserver.ServerApplication;
 
 @SpringBootTest(classes = ServerApplication.class)
